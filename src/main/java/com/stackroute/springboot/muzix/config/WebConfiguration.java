9be80c18ab89c2b4a042457a98d85c50f2610e0f
velsuripoornima <velsuripoornima59@gmail.com>
2019-07-24 10:15:13 +0530

    h2 console

--- src/main/java/com/stackroute/springboot/muzix/config/WebConfiguration.java
+++ src/main/java/com/stackroute/springboot/muzix/config/WebConfiguration.java
@@ -1,4 +1,17 @@
 package com.stackroute.springboot.muzix.config;
 
+import org.apache.catalina.servlets.WebdavServlet;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2ServletRegistration(){
+        ServletRegistrationBean registrationBean=new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
 }
